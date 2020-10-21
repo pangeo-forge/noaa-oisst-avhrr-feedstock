53b1f7c740c17e81ab1ee93c84f859a338d073c1
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-10-21 16:06:50 -0500

    set repo

--- recipe/pipeline.py
+++ recipe/pipeline.py
@@ -85,8 +85,8 @@ def consolidate_metadata(writes: List[str], target: str) -> None:
 class Pipeline(AbstractPipeline):
 
     # Pipeline constants
-    repo = "pangeo-forge/noaa-oisst-avhrr-feedstock/"
-    name = "noaa-oisst-avhrr"
+    repo = "TomAugspurger/noaa-oisst-avhrr-feedstock/"
+    name = "noaa-oisst-avhrr-tom"
     concat_dim = "time"
     files_per_chunk = 5
 
